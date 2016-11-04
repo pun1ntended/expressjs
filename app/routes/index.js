6db5ca7609df926979af85d5df2dc7915381171d
Steven McGrath <mcgrath.steven7@gmail.com>
2016-11-04 13:59:29 -0400

    finished chapter 03_03b

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+  let data = req.app.get('appData');
+  let pagePhotos = [];
 
+  data.speakers.forEach((speaker) => {
+    pagePhotos = pagePhotos.concat(speaker.artwork);
+  });
   res.render('index', {
     pageTitle: 'Home',
+    artwork: pagePhotos,
     pageID: 'home'
   });
 
