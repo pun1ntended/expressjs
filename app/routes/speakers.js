adae387aa45a3627d81c63e2a25cd257d9048d2a
Steven McGrath <mcgrath.steven7@gmail.com>
2016-11-04 12:34:11 -0400

    finished chapter 02_03b

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -4,15 +4,17 @@ var router = express.Router();
 router.get('/speakers', function(req, res) {
   var info = '';
   var dataFile = req.app.get('appData');
-  dataFile.speakers.forEach(function(item) {
+  dataFile.speakers.forEach(function(speaker) {
     info += `
     <li>
-      <h2>${item.name}</h2>
-      <p>${item.summary}</p>
+      <h2>${speaker.name}</h2>
+      <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker">
+      <p>${speaker.summary}</p>
     </li>
     `;
   });
   res.send(`
+      <link rel="stylesheet" type="text/css" href="/css/style.css">
       <h1>Roux Academy Meetups</h1>
       ${info}
   `);
@@ -22,8 +24,10 @@ router.get('/speakers/:speakerid', function(req, res) {
   var dataFile = req.app.get('appData');
   var speaker = dataFile.speakers[req.params.speakerid];
   res.send(`
+      <link rel="stylesheet" type="text/css" href="/css/style.css">
       <h1>${speaker.title}</h1>
       <h2>with ${speaker.name}</h2>
+      <img src="/images/speakers/${speaker.shortname}_tn.jpg">
       <p>${speaker.summary}</p>
   `);
 });
