99eec0df67758a6ba38a5df92573066bcd2070ed
Steven McGrath <mcgrath.steven7@gmail.com>
2016-11-04 16:50:07 -0400

    finished chapter 04_01b

---
+++ app/routes/feedback.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Sven on 11/4/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/feedback', function(req, res) {
+
+  res.render('feedback', {
+    pageTitle: 'Feedback',
+    pageID: 'feedback'
+  });
+
+});
+
+module.exports = router;
